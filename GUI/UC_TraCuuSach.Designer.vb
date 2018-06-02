f0a35589f688fb1bb804ade3f6dec4d2a5edb22d
Tiến Đặng Minh <16521215@ms.uit.edu.vn>
2018-06-02 12:07:13 +0700

    Update Tra cứu sách

--- GUI/UC_TraCuuSach.Designer.vb
+++ GUI/UC_TraCuuSach.Designer.vb
@@ -22,8 +22,8 @@ Partial Class UC_TraCuuSach
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
-        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle5 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle6 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Me.dgv_ListSach = New System.Windows.Forms.DataGridView()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.cbb_TheLoai = New System.Windows.Forms.ComboBox()
@@ -41,28 +41,28 @@ Partial Class UC_TraCuuSach
         Me.dgv_ListSach.AllowUserToResizeRows = False
         Me.dgv_ListSach.BackgroundColor = System.Drawing.SystemColors.Control
         Me.dgv_ListSach.BorderStyle = System.Windows.Forms.BorderStyle.None
-        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
-        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control
-        DataGridViewCellStyle1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText
-        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
-        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
-        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
-        Me.dgv_ListSach.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        DataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.dgv_ListSach.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle5
         Me.dgv_ListSach.EnableHeadersVisualStyles = False
         Me.dgv_ListSach.Location = New System.Drawing.Point(55, 189)
         Me.dgv_ListSach.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.dgv_ListSach.MultiSelect = False
         Me.dgv_ListSach.Name = "dgv_ListSach"
         Me.dgv_ListSach.ReadOnly = True
-        DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
-        DataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control
-        DataGridViewCellStyle2.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        DataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText
-        DataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight
-        DataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText
-        DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
-        Me.dgv_ListSach.RowHeadersDefaultCellStyle = DataGridViewCellStyle2
+        DataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle6.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.dgv_ListSach.RowHeadersDefaultCellStyle = DataGridViewCellStyle6
         Me.dgv_ListSach.RowTemplate.Height = 24
         Me.dgv_ListSach.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
         Me.dgv_ListSach.Size = New System.Drawing.Size(860, 468)
@@ -81,6 +81,7 @@ Partial Class UC_TraCuuSach
         '
         'cbb_TheLoai
         '
+        Me.cbb_TheLoai.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cbb_TheLoai.FormattingEnabled = True
         Me.cbb_TheLoai.Location = New System.Drawing.Point(729, 68)
         Me.cbb_TheLoai.Name = "cbb_TheLoai"
--- GUI/UC_TraCuuSach.vb
+++ GUI/UC_TraCuuSach.vb
@@ -7,6 +7,7 @@ Public Class UC_TraCuuSach
     Private sachBUS As New Sach_BUS()
     Private sachDTO As Sach_DTO
     Private Res As Result
+    Private listSach As List(Of Sach_DTO)
 
     Public Sub New()
 
@@ -39,5 +40,115 @@ Public Class UC_TraCuuSach
     Private Sub UC_TraCuuSach_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ReloadTheLoai()
 
+
+        Reload_DataGridViewListSach()
+
     End Sub
+
+
+
+    Private Sub Reload_DataGridViewListSach()
+
+        Res = sachBUS.SelectALL_ListSach()
+
+        If (Res.FlagResult = False) Then
+            MessageBox.Show(Res.ApplicationMessage + Environment.NewLine + Res.SystemMessage, "Xảy ra lỗi!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        listSach = CType(Res.Obj1, List(Of Sach_DTO))
+
+        dgv_ListSach.Columns.Clear()
+
+        dgv_ListSach.DataSource = listSach
+
+
+
+
+
+        dgv_ListSach.Columns("MaSach1").HeaderText = "Mã"
+        dgv_ListSach.Columns("MaSach1").DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter ' Căn giữa nội dung
+
+
+        dgv_ListSach.Columns("TenSach1").HeaderText = "Tên sách"
+        dgv_ListSach.Columns("TheLoai1").HeaderText = "Thể loại"
+        dgv_ListSach.Columns("TacGia1").HeaderText = "Tác giả"
+
+        With dgv_ListSach.Columns("SoLuongTon1")
+            .HeaderText = "Tồn"
+            .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter ' Căn giữa nội 
+        End With
+
+        With dgv_ListSach.Columns("DonGia1")
+            .HeaderText = "Đơn giá"
+            .DefaultCellStyle.Alignment = DataGridViewContentAlignment.BottomRight
+        End With
+
+
+
+
+        Dim rong As Double = dgv_ListSach.Width
+
+
+        dgv_ListSach.Columns("MaSach1").Width = rong * 0.1
+        dgv_ListSach.Columns("TenSach1").Width = rong * 0.37 - 20
+        dgv_ListSach.Columns("TheLoai1").Width = rong * 0.15
+        dgv_ListSach.Columns("TacGia1").Width = rong * 0.2
+        dgv_ListSach.Columns("SoLuongTon1").Width = rong * 0.08
+        dgv_ListSach.Columns("DonGia1").Width = rong * 0.1
+
+
+
+
+        dgv_ListSach.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(44, 62, 80)
+        dgv_ListSach.ColumnHeadersDefaultCellStyle.ForeColor = Color.White
+
+        dgv_ListSach.RowHeadersVisible = False
+
+        'dgv_ListSach.EnableHeadersVisualStyles = False
+
+
+    End Sub
+
+
+    Private Sub txt_TimKiem_Click(sender As Object, e As EventArgs) Handles txt_TimKiem.Click
+        txt_TimKiem.Text = ""
+    End Sub
+
+    Private Sub txt_TimKiem_Leave(sender As Object, e As EventArgs) Handles txt_TimKiem.Leave
+        txt_TimKiem.Text = "Tìm kiếm bằng Mã Sách hoặc Tên sách..."
+    End Sub
+
+    Private Sub txt_TimKiem_TextChanged(sender As Object, e As EventArgs) Handles txt_TimKiem.TextChanged
+
+
+        If (txt_TimKiem.Text = "") Then
+            Reload_DataGridViewListSach()
+            Return
+        End If
+
+
+        If (txt_TimKiem.Text = "Tìm kiếm bằng Mã Sách hoặc Tên sách...") Then
+            Return
+        End If
+
+
+
+        Res = sachBUS.SelectALL_ListSachByStringMaSachTenSach(txt_TimKiem.Text)
+
+        If (Res.FlagResult = False) Then
+            MessageBox.Show(Res.ApplicationMessage + Environment.NewLine + Res.SystemMessage, "Xảy ra lỗi!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        listSach = CType(Res.Obj1, List(Of Sach_DTO))
+
+
+
+        dgv_ListSach.DataSource = listSach
+
+
+
+    End Sub
+
 End Class
